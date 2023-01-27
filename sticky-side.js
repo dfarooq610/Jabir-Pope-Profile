35583c24e6b42e9854cfe7cf474faa0728d68d87
Danica Jefferies <danjefferies2001@gmail.com>
2023-01-26 21:33:05 -0500

    smaller jpegs

--- sticky-side.js
+++ sticky-side.js
@@ -1,41 +1,41 @@
-      // using d3 for convenience
-      var main = document.querySelector("main");
-      var scrolly = main.querySelector(".scrolly");
-      var sticky = scrolly.querySelector(".sticky-thing");
-      var article = scrolly.querySelector("article");
-      var steps = article.querySelectorAll(".step");
+    // using d3 for convenience
+    var main = document.querySelector("main");
+    var scrolly = main.querySelector(".scrolly");
+    var sticky = scrolly.querySelector(".sticky-thing");
+    var article = scrolly.querySelector("article");
+    var steps = article.querySelectorAll(".step");
 
-      // initialize the scrollama
-      var scroller = scrollama();
+    // initialize the scrollama
+    var scroller = scrollama();
 
-      // scrollama event handlers
-      function handleStepEnter(response) {
-        // response = { element, direction, index }
-        var el = response.element;
+    // scrollama event handlers
+    function handleStepEnter(response) {
+    // response = { element, direction, index }
+    var el = response.element;
 
-        // remove is-active from all steps
-        // then add is-active to this step
-        steps.forEach(step => step.classList.remove('is-active'));
-        el.classList.add('is-active');
+    // remove is-active from all steps
+    // then add is-active to this step
+    steps.forEach(step => step.classList.remove('is-active'));
+    el.classList.add('is-active');
 
-        // update graphic based on step
-        sticky.querySelector("p").innerText = el.dataset.step;
-      }
+    // update graphic based on step
+    sticky.querySelector("p").innerText = el.dataset.step;
+    }
 
 
-      // just comment out debug: true to make lines go away
-      function init() {
-        scroller
-          .setup({
-            step: "#scrolly article .step",
-            offset: 0.33,
-            debug: true
-          })
-          .onStepEnter(handleStepEnter);
+    // just comment out debug: true to make lines go away
+    function init() {
+    scroller
+        .setup({
+        step: "#scrolly article .step",
+        offset: 0.33,
+        debug: true
+        })
+        .onStepEnter(handleStepEnter);
 
-        // setup resize event
-        window.addEventListener("resize", scroller.resize);
-      }
+    // setup resize event
+    window.addEventListener("resize", scroller.resize);
+    }
 
-      // kick things off
-      init();
+    // kick things off
+    init();
